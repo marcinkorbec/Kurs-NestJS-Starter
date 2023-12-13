73e3cb8299fbc53b19b13813a94d58c0611b1a7e
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2023-12-13 17:43:13 +0100

    update appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TimingInterceptor } from './timing.decorator';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TestowyModule } from './testowy/testowy.module';
-import { FoxController } from './fox/fox.controller';
-import { RequestController } from './request/request.controller';
-import { FoxService } from './fox/fox.service';
 import { ShopController } from './shop/shop.controller';
 import { ShopService } from './shop/shop.service';
 import { BasketController } from './basket/basket.controller';
@@ -17,16 +12,14 @@ import { BasketModule } from './basket/basket.module';
 @Module({
     imports: [
         // TypeOrmModule.forRoot(),
-        TestowyModule,
         BasketModule],
-    controllers: [AppController, FoxController, RequestController, ShopController, BasketController],
+    controllers: [AppController, ShopController, BasketController],
     providers: [
         AppService,
         {
             provide: APP_INTERCEPTOR,
             useClass: TimingInterceptor,
         },
-        FoxService,
         ShopService,
         BasketService
     ],
