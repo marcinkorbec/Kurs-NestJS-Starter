f884fd93873df8b1b176b86faee58f5a24cf3c3c
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2023-12-14 20:09:43 +0100

    adding typeorm to skopmodule and adding ShopItem type

--- src/shop/shop.module.ts
+++ src/shop/shop.module.ts
@@ -2,9 +2,11 @@ import { Module, forwardRef } from '@nestjs/common';
 import { ShopController } from './shop.controller';
 import { ShopService } from './shop.service';
 import { BasketModule } from 'src/basket/basket.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShopItem } from './shop-item.entity';
 
 @Module({
-    imports: [forwardRef(() => BasketModule)],
+    imports: [forwardRef(() => BasketModule), TypeOrmModule.forFeature([ShopItem])],
     controllers: [ShopController],
     providers: [ShopService],
     exports: [ShopService]
