a9d581b80d15d35ae9b7f366b612564fb7ed0419
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2023-12-14 16:52:47 +0100

    define database

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { BasketModule } from './basket/basket.module';
 import { ShopModule } from './shop/shop.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
     imports: [
+        TypeOrmModule.forRoot(),
         BasketModule,
         ShopModule,
         UsersModule
