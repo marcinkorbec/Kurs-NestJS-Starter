7236b31446073074cd9536ca301ef608541debf4
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2023-12-15 23:01:55 +0100

    update columns

--- src/shop/shop-item.entity.ts
+++ src/shop/shop-item.entity.ts
@@ -5,13 +5,13 @@ export class ShopItem {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @Column({ length: 60 })
+    @Column({ length: 70 })
     name: string;
 
     @Column({ type: "float", precision: 6, scale: 2 })
     priceNet: number;
 
-    @Column({ length: 10000, default: '(brak)' })
+    @Column({ length: 10000, default: '(brak opisu)' })
     description: string;
 
     @Column()
