c1b29c5dc2242fe3b4f97a41526886e5f7c8340d
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2023-12-19 20:49:46 +0100

    refactor

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { GetListOfProducts, ShopService } from './shop.service';
 import { ShopItem } from './shop-item.entity';
 
@@ -6,14 +6,24 @@ import { ShopItem } from './shop-item.entity';
 export class ShopController {
     constructor(private shopService: ShopService) { }
 
-    @Get()
+    @Get('/')
     async getItems(@Query('page') page: number, @Query('limit') limit: number): Promise<{ items: GetListOfProducts, maxPages: number }> {
         const { items, maxPages } = await this.shopService.getObjects(page, limit);
         return { items, maxPages };
     }
 
-    @Get('/:id')
+    @Get('/one/:id')
     async getItem(@Param('id') id: string): Promise<ShopItem> {
         return this.shopService.getObject(id);
+    }
+
+    @Post('/new-product')
+    async createProduct(@Body() product: ShopItem): Promise<ShopItem> {
+        return this.shopService.createProduct(product);
+    }
+
+    @Get('/test')
+    async getItemsWithPrice2(): Promise<ShopItem[]> {
+        return this.shopService.getItemsWithPriceGreaterThanTwo();
     }
 }
