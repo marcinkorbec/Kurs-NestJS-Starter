68e4008cd55e0848fb481c1b2f71f11674f43821
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2023-12-19 20:50:14 +0100

    update service

--- src/shop/shop.service.ts
+++ src/shop/shop.service.ts
@@ -3,6 +3,7 @@ import { BasketService } from 'src/basket/basket.service';
 import { GetListOfProducts, ShopItem } from './shop-item.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ShopItemDetails } from './shop-item-details.entity';
 
 
 
@@ -12,12 +13,15 @@ export class ShopService {
     constructor(
         @Inject(forwardRef(() => BasketService)) private basketService: BasketService,
         @InjectRepository(ShopItem)
-        private productRepository: Repository<ShopItem>
+        private productRepository: Repository<ShopItem>,
+        @InjectRepository(ShopItemDetails)
+        private productDetailsRepository: Repository<ShopItemDetails>
     ) { }
 
 
-    async getObjects(page = 1, limit = 5): Promise<{ items: GetListOfProducts, maxPages: number }> {
+    async getObjects(page = 1, limit = 20): Promise<{ items: GetListOfProducts, maxPages: number }> {
         const [items, totalItems] = await this.productRepository.findAndCount({
+            relations: ['details', 'mainProduct'],
             skip: (page - 1) * limit,
             take: limit
         });
@@ -25,6 +29,46 @@ export class ShopService {
         const maxPages = Math.ceil(totalItems / limit);
 
         return { items, maxPages };
+    }
+
+    // async getItemsWithPriceGreaterThanTwo(page: number = 1, limit: number = 5): Promise<{ data: ShopItem[], count: number }> {
+    //     const queryBuilder = this.productRepository.createQueryBuilder('product');
+
+    //     queryBuilder.where('product.priceNet > :price', { price: 1 });
+
+    //     const skippedItems = (page - 1) * limit;
+    //     queryBuilder.skip(skippedItems);
+    //     queryBuilder.take(limit);
+
+    //     const [data, count] = await queryBuilder.getManyAndCount();
+
+    //     return { data, count };
+    // }
+
+    async getItemsWithPriceGreaterThanTwo(): Promise<any[]> {
+        const queryBuilder = this.productRepository.createQueryBuilder('product');
+
+        queryBuilder.where('product.priceNet > :price', { price: 2 });
+
+        const items = await queryBuilder.getMany();
+
+        return items;
+    }
+
+    async createProduct(product: ShopItem): Promise<ShopItem> {
+        const newProduct = await this.productRepository.create(product);
+        await this.productRepository.save(newProduct);
+
+        const details = new ShopItemDetails();
+        details.color = 'niebieski';
+        details.width = 20;
+        await this.productDetailsRepository.save(details);
+
+
+        newProduct.details = details;
+        await this.productRepository.save(newProduct);
+
+        return newProduct;
     }
 
     async getObject(id: string): Promise<ShopItem> {
