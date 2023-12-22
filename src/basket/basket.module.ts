b5dc44e238e4c7acad9b9dea2d002ec208347e7d
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2023-12-22 17:04:09 +0100

    update.basket module

--- src/basket/basket.module.ts
+++ src/basket/basket.module.ts
@@ -2,9 +2,11 @@ import { Module, forwardRef } from '@nestjs/common';
 import { BasketService } from './basket.service';
 import { BasketController } from './basket.controller';
 import { ShopModule } from 'src/shop/shop.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BasketItem } from './basket-item.entity';
 
 @Module({
-    imports: [forwardRef(() => ShopModule)],
+    imports: [forwardRef(() => ShopModule), TypeOrmModule.forFeature([BasketItem])],
     controllers: [BasketController],
     providers: [BasketService],
     exports: [BasketService]
