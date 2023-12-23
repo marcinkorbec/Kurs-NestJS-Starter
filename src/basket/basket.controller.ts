4c69290e40b3bdc82ab2810479acc97790eef4d5
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2023-12-23 21:48:44 +0100

    refactor and reworking endpoints with adding a user

--- src/basket/basket.controller.ts
+++ src/basket/basket.controller.ts
@@ -1,15 +1,18 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe, Delete, Param, ParseIntPipe, Get, HttpStatus, HttpException } from '@nestjs/common';
+import { Body, Controller, Post, Delete, Param, Get, HttpStatus, HttpException, Req } from '@nestjs/common';
 import { BasketService } from './basket.service';
 import { CreateBasketItemDto } from '../shared/DTOs/create-basket-item.dto';
+import { Request } from 'express';
+
 
 @Controller('basket')
 export class BasketController {
     constructor(private readonly basketService: BasketService) { }
 
-    @Post('add')
-    async addToBasket(@Body() createBasketItemDto: CreateBasketItemDto) {
+    @Post('/add')
+    async addToBasket(@Req() req: Request, @Body() createBasketItemDto: CreateBasketItemDto) {
+        const userId = req.user?.id;
         try {
-            const newItem = await this.basketService.addToBasket(createBasketItemDto);
+            const newItem = await this.basketService.addToBasket(userId, createBasketItemDto);
             return newItem;
         } catch (error) {
             throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
@@ -17,9 +20,10 @@ export class BasketController {
     }
 
     @Delete('remove/:id')
-    async removeFromBasket(@Param('id') id: number) {
+    async removeFromBasket(@Req() req: Request, @Param('id') id: string) {
+        const userId = req.user.id;
         try {
-            await this.basketService.removeFromBasket(id);
+            await this.basketService.removeFromBasket(userId, id);
             return { message: 'Item removed successfully' };
         } catch (error) {
             throw new HttpException('Item not found', HttpStatus.NOT_FOUND);
@@ -27,23 +31,26 @@ export class BasketController {
     }
 
     @Delete('clear')
-    async clearBasket() {
-        await this.basketService.clearBasket();
+    async clearBasket(@Req() req: Request) {
+        const userId = req.user.id;
+        await this.basketService.clearBasket(userId);
         return { message: 'Basket cleared successfully' };
     }
 
-    @Get()
-    async getBasket() {
-        return await this.basketService.getBasket();
-    }
-
     @Get('total-price')
-    async getTotalPrice() {
+    async getTotalPrice(@Req() req: Request) {
+        const userId = req.user.id;
         try {
-            const totalPrice = await this.basketService.getTotalPrice();
+            const totalPrice = await this.basketService.getTotalPrice(userId);
             return { totalPrice };
         } catch (error) {
             throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @Get()
+    async getBasket(@Req() req: Request) {
+        const userId = req.user.id;
+        return await this.basketService.getBasket(userId);
     }
 }
--- src/basket/basket.service.ts
+++ src/basket/basket.service.ts
@@ -1,44 +1,55 @@
 import { Inject, Injectable, NotFoundException, forwardRef } from '@nestjs/common';
 import { ShopService } from '../shop/shop.service';
 import { CreateBasketItemDto } from '../shared/DTOs/create-basket-item.dto';
-// import { BasketItem } from 'src/shared';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BasketItem } from './basket-item.entity';
 import { Repository } from 'typeorm';
+import { User } from 'src/users/users.entity';
 
 @Injectable()
 export class BasketService {
-    private basket: CreateBasketItemDto[] = [];
+
 
     constructor(
         @Inject(forwardRef(() => ShopService)) private shopService: ShopService,
         @InjectRepository(BasketItem) private basketItemRepository: Repository<BasketItem>,
+        @InjectRepository(User) private userRepository: Repository<User>,
     ) { }
 
-    async addToBasket(itemDto: CreateBasketItemDto): Promise<BasketItem> {
-        const item = this.basketItemRepository.create(itemDto);
+    async addToBasket(userId: string, itemDto: CreateBasketItemDto): Promise<BasketItem> {
+        const user = await this.userRepository.findOne(userId);
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        const item = this.basketItemRepository.create({
+            ...itemDto,
+            user: user
+        });
         await this.basketItemRepository.save(item);
         return item;
     }
 
-    async removeFromBasket(id: number): Promise<void> {
-        const result = await this.basketItemRepository.delete(id);
-        if (result.affected === 0) {
+    async removeFromBasket(userId: string, itemId: string): Promise<void> {
+        const item = await this.basketItemRepository.findOne({ where: { id: itemId, user: { id: userId } } });
+        if (!item) {
             throw new NotFoundException('Item not found in basket');
         }
+        await this.basketItemRepository.remove(item);
     }
 
-    async clearBasket(): Promise<void> {
-        await this.basketItemRepository.clear();
+    async clearBasket(userId: string): Promise<void> {
+        await this.basketItemRepository.delete({ user: { id: userId } });
     }
 
-    async getBasket(): Promise<BasketItem[]> {
-        return await this.basketItemRepository.find();
+    async getBasket(userId: string): Promise<BasketItem[]> {
+        return await this.basketItemRepository.find({
+            where: { user: { id: userId } }
+        });
     }
 
-    async getTotalPrice(): Promise<number> {
+    async getTotalPrice(userId: string): Promise<number> {
         let totalPrice = 0;
-        const basketItems = await this.getBasket();
+        const basketItems = await this.getBasket(userId);
         for (const item of basketItems) {
             const netPrice = await this.shopService.getNetPrice(item.name);
             totalPrice += netPrice * 1.23; // assuming VAT is 23%
@@ -46,21 +57,21 @@ export class BasketService {
         return totalPrice;
     }
 
-    getAlternativeBasket(): { alternativeBasket: BasketItem[], removedItems: BasketItem[] } {
-        const alternativeBasket = [];
-        const removedItems = [];
+    // getAlternativeBasket(): { alternativeBasket: BasketItem[], removedItems: BasketItem[] } {
+    //     const alternativeBasket = [];
+    //     const removedItems = [];
 
-        for (const item of this.basket) {
-            try {
-                this.shopService.getNetPrice(item.name);
-                alternativeBasket.push(item);
-            } catch (error) {
-                if (error instanceof NotFoundException) {
-                    removedItems.push(item);
-                }
-            }
-        }
+    //     for (const item of this.basket) {
+    //         try {
+    //             this.shopService.getNetPrice(item.name);
+    //             alternativeBasket.push(item);
+    //         } catch (error) {
+    //             if (error instanceof NotFoundException) {
+    //                 removedItems.push(item);
+    //             }
+    //         }
+    //     }
 
-        return { alternativeBasket, removedItems };
-    }
+    //     return { alternativeBasket, removedItems };
+    // }
 }
