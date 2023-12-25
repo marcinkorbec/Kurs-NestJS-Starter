32b24de8d38359905fa4cba512cb4651d0209938
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2023-12-25 20:06:37 +0100

    add guard to controller

--- src/administrator/administrator.controller.ts
+++ src/administrator/administrator.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AdministratorService } from './administrator.service';
+import { PasswordProtectGuard } from 'src/guards/password-protect.guard';
 
 @Controller('admin')
 export class AdministratorController {
     constructor(private readonly administratorService: AdministratorService) { }
 
+    @UseGuards(PasswordProtectGuard)
     @Get('/baskets')
     async getAllBasketsWithItems() {
         return await this.administratorService.getAllBasketsWithItems();
     }
 
+    @UseGuards(PasswordProtectGuard)
     @Get('/users')
     async getAllUsers() {
         return await this.administratorService.getAllUsers();
