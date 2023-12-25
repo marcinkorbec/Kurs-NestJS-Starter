13274e434f78b79e872879eb8dd5cd5ea13b84d4
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2023-12-25 11:17:45 +0100

    add method controller

--- src/administrator/administrator.controller.ts
+++ src/administrator/administrator.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AdministratorService } from './administrator.service';
 
-@Controller('administrator')
+@Controller('admin')
 export class AdministratorController {
     constructor(private readonly administratorService: AdministratorService) { }
 
     @Get('/baskets')
     async getAllBasketsWithItems() {
         return await this.administratorService.getAllBasketsWithItems();
+    }
+
+    @Get('/users')
+    async getAllUsers() {
+        return await this.administratorService.getAllUsers();
     }
 }
