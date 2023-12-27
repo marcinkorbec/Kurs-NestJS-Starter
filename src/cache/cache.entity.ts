685e226902bddfb780453fed96f6e3ab98b070a1
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2023-12-27 19:51:14 +0100

    Revert "Revert "add new migration""
    This reverts commit 01af486d236e95b6f7f7f7f44de90c178be58057.

---
+++ src/cache/cache.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Cache {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    key: string;
+
+    @Column('json')
+    value: any;
+
+    @Column()
+    controllerName: string;
+
+    @Column()
+    actionName: string;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt: Date;
+}
