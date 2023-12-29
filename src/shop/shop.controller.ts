50c63aa5dc444b8500c0cdcf358de71b24468de7
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2023-12-29 23:04:07 +0100

    update

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Param, Post, Query, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { GetListOfProducts, ShopService } from './shop.service';
 import { ShopItem } from './shop-item.entity';
+import { AddProductDto } from './DTO/add-product.dto';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { storageDir } from 'src/utils/storage';
+import * as path from 'path';
 
 @Controller('shop')
 export class ShopController {
@@ -18,9 +22,23 @@ export class ShopController {
     }
 
     @Post('/new-product')
-    async createProduct(@Body() product: ShopItem): Promise<ShopItem> {
+    @UseInterceptors(
+        FileFieldsInterceptor([
+            { name: 'photo', maxCount: 1 }
+        ], {
+            dest: path.join(storageDir(), 'product-photos'),
+        })
+    )
+    async createProduct(
+        @Body() product: AddProductDto
+    ): Promise<ShopItem> {
+        if (!files.photo || files.photo.length === 0) {
+            throw new BadRequestException('Photo file is required.');
+        }
+        const file = files.photo[0];
         return this.shopService.createProduct(product);
     }
+
 
     @Get('/test/:page')
     async getItemsWithPriceGreaterThanTwo(@Param('page') page: number, @Query('limit') limit: number): Promise<{ data: ShopItem[], count: number }> {
