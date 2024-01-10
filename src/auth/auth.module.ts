0518a27c174bbe24229c6ecd2605651659c367e5
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2024-01-10 21:31:15 +0100

    fix misssing import

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
+import { HashingModule } from 'src/hashing-pwd/hashing-pwd.module';
 
 @Module({
+  imports: [HashingModule],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
   exports: [JwtStrategy]
