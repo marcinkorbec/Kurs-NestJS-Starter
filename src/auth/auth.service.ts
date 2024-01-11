2927b33d9fd46d1b715d1f190247ef62de03a475
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2024-01-11 23:56:22 +0100

    Add token generation and authentication logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,8 @@ import { Response } from 'express';
 import { User } from 'src/users/users.entity';
 import { AuthLoginDto } from './dto/auth-login.dto';
 import { HashingService } from '../hashing-pwd/hashing-pwd.service';
-
+import { v4 as uuid } from 'uuid';
+import { sign } from 'jsonwebtoken';
 
 @Injectable()
 export class AuthService {
@@ -12,12 +13,40 @@ export class AuthService {
         private hashingService: HashingService
     ) { }
 
+    private createToken(currentTokenId: string): { accessToken: string, expiresIn: number } {
+        const payload = { id: currentTokenId };
+        const expiresIn = 60 * 60 * 24 * 30;
+        const accessToken = sign(payload, 'iwfygluylsycbidcwIFJFAIFVBSDCIUSDFAIUSDVHIAUSDVNDAFYVUSVSsdvn', { expiresIn });
+        return {
+            accessToken,
+            expiresIn
+        };
+    }
+
+    private async generateToken(user: User): Promise<string> {
+        let token;
+        let userWithThisToken = null;
+        do {
+            token = uuid();
+            userWithThisToken = await User.findOne({ currentTokenId: token });
+        } while (!!userWithThisToken);
+        user.currentTokenId = token;
+        await user.save();
+        return token;
+    }
+
     async login(req: AuthLoginDto, res: Response): Promise<any> {
         try {
-            const user = await User.findOne({ login: req.login, password: req.password });
+            const user = await User.findOne({ login: req.login });
             if (!user) {
                 return res.json({ message: 'Invalid credentials' });
             }
+
+            const isPasswordValid = await this.hashingService.comparePassword(req.password, user.password);
+            if (!isPasswordValid) {
+                return res.json({ message: 'Invalid credentials' });
+            }
+
             const token = await this.createToken(await this.generateToken(user));
 
             return res
