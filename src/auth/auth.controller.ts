4aa65b3c8e5680b41ea3946fd3a10323dd71b611
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2024-01-12 21:33:48 +0100

    Add logout endpoint with JWT authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Post, Body, Res } from '@nestjs/common';
+import { Controller, Post, Body, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Response } from 'express';
 import { AuthLoginDto } from './dto/auth-login.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/users/users.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +12,11 @@ export class AuthController {
     @Post('/login')
     async login(@Body() req: AuthLoginDto, @Res() res: Response) {
         return await this.authService.login(req, res);
+    }
+
+    @Post('/logout')
+    @UseGuards(AuthGuard('jwt'))
+    async logout(@Res() res: Response, @User() user: User) {
+        return await this.authService.logout(res);
     }
 }
